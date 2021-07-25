3dea9b8bc5d84473874e671d15ba52dd4e9f7bc6
Dzmitry85 <dpinchuk85@gmail.com>
2021-07-25 17:46:27 +0300

    Практическое задание-4

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import logo from '../../../src/logo.svg';
-import TextField from '@material-ui/core/TextField'
-import usePrevious from '../../hooks/usePrevious'
-import Message from '../Message/Message'
+// import TextField from '@material-ui/core/TextField'
+// import usePrevious from '../../hooks/usePrevious'
+// import Message from '../Message/Message'
 import { AUTHORS } from './constants'
-// 
+import Chat from '../Chat/Chatitem'
 import SimpleList from '../SimpleList';
 // 
 import './App.css'
@@ -12,80 +12,80 @@ import './App.css'
 
 
 function App() {
-    const [messageList, setMessageList] = React.useState([])
-    const [inputValue, setInputValue] = React.useState('')
+    // const [messageList, setMessageList] = React.useState([])
+    // const [inputValue, setInputValue] = React.useState('')
 
-    const timer = React.useRef(null)
+    // const timer = React.useRef(null)
 
-    const prevMessageList = usePrevious(messageList)
+    // const prevMessageList = usePrevious(messageList)
 
-    React.useEffect(() => {
-        if (
-            prevMessageList?.length < messageList.length &&
-            messageList[messageList.length - 1].author !== AUTHORS.BOT
-        ) {
-            timer.current = setTimeout(
-                () =>
-                    setMessageList((currentMessageList) => [
-                        ...currentMessageList,
-                        { author: AUTHORS.BOT, text: 'Сообщение бота' },
-                    ]),
-                1500
-            )
-        }
-    }, [messageList, prevMessageList])
+    // React.useEffect(() => {
+    //     if (
+    //         prevMessageList?.length < messageList.length &&
+    //         messageList[messageList.length - 1].author !== AUTHORS.BOT
+    //     ) {
+    //         timer.current = setTimeout(
+    //             () =>
+    //                 setMessageList((currentMessageList) => [
+    //                     ...currentMessageList,
+    //                     { author: AUTHORS.BOT, text: 'Сообщение бота' },
+    //                 ]),
+    //             1500
+    //         )
+    //     }
+    // }, [messageList, prevMessageList])
 
-    React.useEffect(() => {
-        return () => {
-            clearTimeout(timer.current)
-        }
-    }, [])
+    // React.useEffect(() => {
+    //     return () => {
+    //         clearTimeout(timer.current)
+    //     }
+    // }, [])
 
-    const handleMessageChange = (e) => {
-        setInputValue(e.target.value)
-    }
+    // const handleMessageChange = (e) => {
+    //     setInputValue(e.target.value)
+    // }
 
-    const handleMessageSubmit = (e) => {
-        e.preventDefault()
+    // const handleMessageSubmit = (e) => {
+    //     e.preventDefault()
 
-        setMessageList((currentMessageList) => [
-            ...currentMessageList,
-            { author: AUTHORS.ME, text: inputValue },
-        ])
-        setInputValue('')
-    }
+    //     setMessageList((currentMessageList) => [
+    //         ...currentMessageList,
+    //         { author: AUTHORS.ME, text: inputValue },
+    //     ])
+    //     setInputValue('')
+    // }
     // 
     
     //   
-    const Chat=()=>(
-<div>
-            <div className="top">
+//     const Chat=()=>(
+// <div>
+//             <div className="top">
            
-                {messageList.map((message, index) => (
-                    <Message
-                        key={index}
-                        text={message.text}
-                        author={message.author}
-                    />
-                ))}
-            </div>
+//                 {messageList.map((message, index) => (
+//                     <Message
+//                         key={index}
+//                         text={message.text}
+//                         author={message.author}
+//                     />
+//                 ))}
+//             </div>
 
-            <form className="app__form bordered" onSubmit={handleMessageSubmit}>
-                <TextField
-                    fullWidth
-                    required
-                    autoFocus
-                    className="child__text-field bordered"
-                    variant="outlined"
-                    label="Сообщение"
-                    placeholder="Введите сообщение"
-                    value={inputValue}
-                    onChange={handleMessageChange}
-                />
-                <button>Отправить</button>
-            </form>
-            </div>
-    )
+//             <form className="app__form bordered" onSubmit={handleMessageSubmit}>
+//                 <TextField
+//                     fullWidth
+//                     required
+//                     autoFocus
+//                     className="child__text-field bordered"
+//                     variant="outlined"
+//                     label="Сообщение"
+//                     placeholder="Введите сообщение"
+//                     value={inputValue}
+//                     onChange={handleMessageChange}
+//                 />
+//                 <button>Отправить</button>
+//             </form>
+//             </div>
+//     )
 
 
     return (
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter} from 'react-router-dom'
 import './index.css'
-import App from './components/App/App'
+// import App from './components/App/App'
+import Router from './Router/Router'
+
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+        {/* <App /> */}
+        <Router/>
+        </BrowserRouter>
+    
     </React.StrictMode>,
     document.getElementById('root')
 )
